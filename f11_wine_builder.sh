e3f22969d6ca3c73bc3987a4d37a2da6a0987ea0
DanielDevBR <dados.linux@gmail.com>
2020-08-02 18:21:15 -0300

    removed wine-stable:i386

--- f11_wine_builder.sh
+++ f11_wine_builder.sh
@@ -24,7 +24,7 @@ wget -c "https://dev.gentoo.org/~sarnex/distfiles/wine/gentoo-wine-patches-${GEN
 tar xf "gentoo-wine-patches-${GENTOO_PATCH_VERSION}.tar.xz" || die "* Cant extract the gentoo patchs"
 
 echo "* Install deps:"
-sudo apt-get -y build-dep wine-development wine-stable:i386 libsdl2 libvulkan1 xz-utils || die "* apt-get error!"
+sudo apt-get -y build-dep wine-development libsdl2 libvulkan1 xz-utils || die "* apt-get error!"
 sudo apt-get -y install libusb-1.0-0-dev libgcrypt20-dev libpulse-dev libudev-dev libsane-dev libv4l-dev libkrb5-dev libgphoto2-dev liblcms2-dev libpcap-dev libcapi20-dev || die "* apt-get error!"
 sudo apt-get -y purge libvulkan-dev libvulkan1 libsdl2-dev libsdl2-2.0-0 --purge --autoremove || die "* apt-get error!"
 sudo apt-get -y clean || die "* apt-get error!"
