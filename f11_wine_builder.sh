ddb85d380f98fd6d3c3dcc5e28ee9e2f12b52fe0
DanielDevBR <dados.linux@gmail.com>
2020-08-02 15:27:22 -0300

    starting repo

---
+++ f11_wine_builder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#GENTOO_PATCH_VERSION="20200523"
+
+#=================================================
+die() { echo >&2 "$*"; exit 1; };
+#=================================================
+
+#echo "* Getting gentoo wine patchs for winegcc.patch:"
+#wget -c "https://dev.gentoo.org/~sarnex/distfiles/wine/gentoo-wine-patches-${GENTOO_PATCH_VERSION}.tar.xz"
+#tar xf "gentoo-wine-patches-${GENTOO_PATCH_VERSION}.tar.xz" || die "* Can't extract the gentoo patchs"
