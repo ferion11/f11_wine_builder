9dda9a6872ad831c08655ca5ab88516042cf0f64
DanielDevBR <dados.linux@gmail.com>
2020-08-02 15:52:51 -0300

    get the versions of gcc and g++ in use

--- f11_wine_builder.sh
+++ f11_wine_builder.sh
@@ -8,3 +8,7 @@ die() { echo >&2 "$*"; exit 1; };
 #echo "* Getting gentoo wine patchs for winegcc.patch:"
 #wget -c "https://dev.gentoo.org/~sarnex/distfiles/wine/gentoo-wine-patches-${GENTOO_PATCH_VERSION}.tar.xz"
 #tar xf "gentoo-wine-patches-${GENTOO_PATCH_VERSION}.tar.xz" || die "* Can't extract the gentoo patchs"
+
+echo "* Versions:"
+gcc --version
+g++ --version
