b957bdff854e3cd94b0563b95e00b5196ad56a04
DanielDevBR <dados.linux@gmail.com>
2020-08-04 22:07:19 -0300

    log message format change

--- deployscript/ubuntu32-deploy.sh
+++ deployscript/ubuntu32-deploy.sh
@@ -38,40 +38,40 @@ echo "deb-src ${CHROOT_MIRROR} ${CHROOT_DISTRO}-proposed main restricted univers
 echo "deb-src ${CHROOT_MIRROR} ${CHROOT_DISTRO}-backports main restricted universe multiverse" >> /etc/apt/sources.list
 
 apt-get -q -y update >/dev/null
-echo "* Install software-properties-common:"
+echo "* Install software-properties-common..."
 apt-get -q -y install software-properties-common apt-utils >/dev/null || die "* apt software-properties-common and apt-utils erro!"
 
 # gcc-9 ppa:
 add-apt-repository ppa:ubuntu-toolchain-r/test >/dev/null
 
-echo "* update, upgrade and dist-upgrade:"
+echo "* update, upgrade and dist-upgrade..."
 apt-get -q -y update >/dev/null
 apt-get -q -y upgrade >/dev/null
 apt-get -q -y dist-upgrade >/dev/null
 
-echo "* Install deps:"
+echo "* Install deps..."
 apt-get -q -y install wget git sudo make cmake gcc-9 g++-9 tar gzip xz-utils bzip2 gawk sed flex bison >/dev/null || die "* apt basic erro!"
 apt-get -q -y install xserver-xorg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libglu1-mesa-dev:i386 libosmesa6-dev:i386 libvulkan-dev:i386 libvulkan1:i386 libpulse-dev:i386 libopenal-dev:i386 libncurses-dev:i386 libgnutls28-dev:i386 libtiff-dev:i386 libldap-dev:i386 libcapi20-dev:i386 libpcap-dev:i386 libxml2-dev:i386 libmpg123-dev:i386 libgphoto2-dev:i386 libsane-dev:i386 libcupsimage2-dev:i386 libkrb5-dev:i386 libgsm1-dev:i386 libxslt1-dev:i386 libv4l-dev:i386 libgstreamer-plugins-base1.0-dev:i386 libudev-dev:i386 libxi-dev:i386 liblcms2-dev:i386 libibus-1.0-dev:i386 libsdl2-dev:i386 ocl-icd-opencl-dev:i386 libxinerama-dev:i386 libxcursor-dev:i386 libxrandr-dev:i386 libxcomposite-dev:i386 libavcodec57:i386 libavcodec-dev:i386 libswresample2:i386 libswresample-dev:i386 libavutil55:i386 libavutil-dev:i386 libusb-1.0-0-dev:i386 libgcrypt20-dev:i386 libasound2-dev:i386 libjpeg8-dev:i386 libldap2-dev:i386 libx11-dev:i386 zlib1g-dev:i386 libcups2:i386 libdbus-1-3:i386 libicu-dev:i386 libncurses5:i386 >/dev/null || die "* main apt erro!"
 apt-get -q -y purge libvulkan-dev libvulkan1 libsdl2-dev libsdl2-2.0-0 --purge --autoremove >/dev/null || die "* apt purge error!"
 # removed  libfaudio0:i386 libfaudio-dev:i386 (building it below), libvkd3d-dev:i386
 
-echo "* compile and install more deps:"
+echo "* compile and install more deps..."
 mkdir "${WORKDIR}/build_libs"
 cd "${WORKDIR}/build_libs" || die "* Cant enter on dir build_libs!"
 
-echo "* downloading SDL2:"
+echo "* downloading SDL2..."
 wget -q "https://www.libsdl.org/release/SDL2-${SDL2_VERSION}.tar.gz"
-echo "* downloading FAudio:"
+echo "* downloading FAudio..."
 wget -q "https://github.com/FNA-XNA/FAudio/archive/${FAUDIO_VERSION}.tar.gz" -O "FAudio-${FAUDIO_VERSION}.tar.gz"
-echo "* downloading Vulkan-Headers:"
+echo "* downloading Vulkan-Headers..."
 wget -q "https://github.com/KhronosGroup/Vulkan-Headers/archive/v${VULKAN_VERSION}.tar.gz" -O "Vulkan-Headers-${VULKAN_VERSION}.tar.gz"
-echo "* downloading Vulkan-Loader:"
+echo "* downloading Vulkan-Loader..."
 wget -q "https://github.com/KhronosGroup/Vulkan-Loader/archive/v${VULKAN_VERSION}.tar.gz" -O "Vulkan-Loader-${VULKAN_VERSION}.tar.gz"
-echo "* downloading SPIRV-Headers:"
+echo "* downloading SPIRV-Headers..."
 wget -q "https://github.com/KhronosGroup/SPIRV-Headers/archive/${SPIRV_VERSION}.tar.gz"  -O "SPIRV-Headers-${SPIRV_VERSION}.tar.gz"
 git clone --depth 1 https://github.com/HansKristian-Work/vkd3d-proton.git
 
-echo "* extracting:"
+echo "* extracting..."
 tar xf "SDL2-${SDL2_VERSION}.tar.gz" || die "* extract tar.gz error!"
 tar xf "FAudio-${FAUDIO_VERSION}.tar.gz" || die "* extract tar.gz error!"
 tar xf "Vulkan-Headers-${VULKAN_VERSION}.tar.gz" || die "* extract tar.gz error!"
@@ -109,17 +109,17 @@ rm -rf "${WORKDIR}/build_libs"
 #==============================================================================
 
 echo "* Wine part:"
-echo "* Getting wine source and patch:"
+echo "* Getting wine source and patch..."
 wget -q "https://dl.winehq.org/wine/source/5.x/wine-${WINE_VERSION}.tar.xz"
 tar xf "wine-${WINE_VERSION}.tar.xz" || die "* cant extract wine!"
 mv "wine-${WINE_VERSION}" "wine-src" || die "* cant rename wine-src!"
 
 wget -q "https://github.com/wine-staging/wine-staging/archive/v${WINE_VERSION}.tar.gz"
 tar xf "v${WINE_VERSION}.tar.gz" || die "* cant extract wine-staging patchs!"
-echo "* Applying staging patchs:"
+echo "* Applying staging patchs..."
 "./wine-staging-${WINE_VERSION}/patches/patchinstall.sh" DESTDIR="${WORKDIR}/wine-src" --all >"${WORKDIR}/staging_patches.txt" || die "* Cant apply the wine-staging patches!"
 
-echo "* Compiling:"
+echo "* Compiling..."
 mkdir "wine-staging"
 cd wine-src || die "* Cant enter on the wine-src dir!"
 #./configure "${WINE_BUILD_OPTIONS}" --prefix "${WORKDIR}/wine-staging"
@@ -128,7 +128,7 @@ make -j"$(nproc)" --no-print-directory || die "* cant make wine!"
 make install --no-print-directory || die "* cant install wine!"
 
 cd "${WORKDIR}/wine-staging" || die "* Cant enter on the wine-staging dir!"
-echo "* Some clean:"
+echo "* Cleaning..."
 rm -r include && rm -r share/applications && rm -r share/man
 echo "* Compressing: wine-staging-${WINE_VERSION}.tar.gz"
 tar czf "${WORKDIR}/wine-staging-${WINE_VERSION}.tar.gz" *
