3717be01ca351532db546f251517b2d3d6eabfa6
DanielDevBR <dados.linux@gmail.com>
2020-08-26 21:45:16 -0300

    update wine-stable deb

--- deployscript/ubuntu32-deploy.sh
+++ deployscript/ubuntu32-deploy.sh
@@ -95,8 +95,8 @@ build_and_install "Vulkan-Loader-${VULKAN_VERSION}"
 build_and_install "SPIRV-Headers-${SPIRV_VERSION}"
 # Need libvkd3d-dev package that refuse to install on bionic, so workaround:
 echo "* compiling and install vkd3d-proton>"
-wget -q https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_5.0.1~bionic_i386.deb
-dpkg -x wine-stable_5.0.1~bionic_i386.deb .
+wget -q https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_5.0.2~bionic_i386.deb
+dpkg -x wine-stable_5.0.2~bionic_i386.deb .
 cp ./opt/wine-stable/bin/widl /usr/bin/ || die "cant copy widl erro!"
 cd vkd3d-proton || die "* Cant enter on vkd3d-proton dir!"
 ./autogen.sh >/dev/null
