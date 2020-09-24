4063e10aee413dbd0db4e2a83c018e3665fb9be0
DanielDevBR <dados.linux@gmail.com>
2020-09-24 18:46:03 -0300

    removed debug exit, added null to apt output, compiling

--- 4_compile_wine32.sh
+++ 4_compile_wine32.sh
@@ -17,7 +17,6 @@ mkdir "wine-inst"
 # compile 32bits
 cd "${WORKDIR}/build32" || die "* Cant enter on the ${WORKDIR}/build32 dir!"
 PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig:/usr/lib32/pkgconfig" ../wine-src/configure --prefix "${WORKDIR}/wine-inst" --disable-tests
-echo "DEBUG EXIT"; exit 1
 make -j"$(nproc)" --no-print-directory || die "* cant make wine32!"
 
 cd "${WORKDIR}" || die "Cant enter on ${WORKDIR} dir!"
