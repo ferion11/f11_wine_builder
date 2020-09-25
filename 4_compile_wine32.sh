67f4420603386b90239403367134da26c1e42b2a
DanielDevBR <dados.linux@gmail.com>
2020-09-25 05:56:21 -0300

    change to new compilation format with GA

---
+++ 4_compile_wine32.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source ./0_variables.sh
+#==============================================================================
+
+#=================================================
+die() { echo >&2 "$*"; exit 1; };
+#=================================================
+#==============================================================================
+cat /etc/issue
+WORKDIR=$(pwd)
+echo "* Working inside ${WORKDIR}"
+
+echo "* Compiling..."
+mkdir "build32"
+mkdir "wine-inst"
+
+# compile 32bits
+cd "${WORKDIR}/build32" || die "* Cant enter on the ${WORKDIR}/build32 dir!"
+PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig:/usr/lib32/pkgconfig" ../wine-src/configure --prefix "${WORKDIR}/wine-inst" --disable-tests
+make -j"$(nproc)" --no-print-directory || die "* cant make wine32!"
+
+cd "${WORKDIR}" || die "Cant enter on ${WORKDIR} dir!"
