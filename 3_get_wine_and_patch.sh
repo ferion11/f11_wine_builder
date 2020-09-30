98b8b49dce71f27bb08d67fd10af65601ac3b48e
DanielDevBR <dados.linux@gmail.com>
2020-09-30 13:04:04 -0300

    added and apply patch revert_removal_libwine.patch

--- 3_get_wine_and_patch.sh
+++ 3_get_wine_and_patch.sh
@@ -22,6 +22,6 @@ tar xf "v${STAGING_VERSION}.tar.gz" || die "* cant extract wine-staging patchs!"
 echo "* Applying patchs..."
 "./wine-staging-${STAGING_VERSION}/patches/patchinstall.sh" DESTDIR="${WORKDIR}/wine-src" --all || die "* Cant apply the wine-staging patches!"
 cd "${WORKDIR}/wine-src" || die "Cant enter on ${WORKDIR}/wine-src dir!"
-#patch -p1 < "${WORKDIR}/patches/timeout_infinite_fix.patch" || die "Cant apply the timeout_infinite_fix.patch!"
+patch -p1 < "${WORKDIR}/patches/revert_removal_libwine.patch" || die "Cant apply the revert_removal_libwine.patch!"
 
 cd "${WORKDIR}" || die "Cant enter on ${WORKDIR} dir!"
